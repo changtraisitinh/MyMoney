9d77032580e6c3bd92b6b156429595ed2d37bdfd
Hiep Nguyen <nguyenhuuhiep1990@gmail.com>
2020-01-08 13:55:15 +0700

    init project

---
+++ MyMoney/Controllers/ReportsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReportsViewController.swift
+//  MyMoney
+//
+//  Created by Admin on 08/01/2020.
+//  Copyright © 2020 changtraisitinh. All rights reserved.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
